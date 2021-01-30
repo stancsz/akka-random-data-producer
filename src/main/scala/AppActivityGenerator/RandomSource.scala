70000a66aa352a63896ccc3c164ffd14dbbc03a9
Stan Chen <stanleychen587@gmail.com>
2021-01-30 04:15:52 -0700

    added base script for outbound

---
+++ src/main/scala/AppActivityGenerator/RandomSource.scala
@@ -0,0 +1,11 @@
+//https://doc.akka.io/docs/akka/current/stream/stream-quickstart.html
+
+import akka.stream._
+import akka.stream.scaladsl._
+import akka.{ Done, NotUsed }
+import akka.actor.ActorSystem
+import akka.util.ByteString
+import scala.concurrent._
+import scala.concurrent.duration._
+import java.nio.file.Paths
+
